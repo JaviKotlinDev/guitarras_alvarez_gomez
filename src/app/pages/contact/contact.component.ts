a7922e6952b303f7df034d0a8e2012c91fd81480
javier.alvarezj <javier.alvarezj@alten.es>
2025-05-20 19:29:16 +0200

    Send mails

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import emailjs from 'emailjs-com';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 
-export class ContactComponent {}
+export class ContactComponent {
+
+formData = {
+    name: '',
+    email: '',
+    message: ''
+  };
+
+  sendEmail(form: any) {
+    if (form.invalid) return;
+
+    emailjs.send('service_okwhk9f', 'template_tvdxbwn', this.formData, 'R3dUaUR_-IAvUbpfX')
+      .then(() => {
+        alert('Mensaje enviado con éxito');
+        form.resetForm();
+      }, (error) => {
+        console.error('Error al enviar el mensaje:', error);
+        alert('Error al enviar el mensaje. Inténtalo más tarde.');
+      });
+  }
+
+}
