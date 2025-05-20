cae27a9c7b1babd36d6080f9e489c677d0daefec
javier.alvarezj <javier.alvarezj@alten.es>
2025-05-20 18:47:17 +0200

    navBar responsive

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, HostListener  } from '@angular/core';
+import { NgIf } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgIf],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 
-export class NavbarComponent {}
+export class NavbarComponent {
+  menuOpen = false;
+  isMobileView = window.innerWidth < 768; // md breakpoint
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.isMobileView = window.innerWidth < 768;
+    if (!this.isMobileView) {
+      this.menuOpen = false;
+    }
+  }
+
+  isMobile() {
+    return this.isMobileView;
+  }
+  
+}
